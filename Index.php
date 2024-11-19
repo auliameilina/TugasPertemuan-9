1117e14e8d8349060bd8ba656820efd550c79c57
auliameilina <auliameilina540@gmail.com>
2024-11-19 22:01:33 +0700

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,22 @@
+<?php
+
+$nama   = "Aulia Meilina";
+$Kelas  = "TI.23.CA.1";
+$Nim    = "312310327";
+$Prodi  = "Tekhnik Informatika";
+
+?>
+
+<h1>
+Saya Adalah 
+<?php echo $nama?>
+</h1>
+<p>
+ <strong>Kelas</strong>=<?php echo $Kelas?>
+</p>
+<p>
+ <strong>Nim</strong>=<?php echo $Nim?>
+</p>
+<p>
+<strong>Prodi</strong>=<?php echo $Prodi?>
+</p>
